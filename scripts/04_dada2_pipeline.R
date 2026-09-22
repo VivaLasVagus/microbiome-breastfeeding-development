c8ab08c4bc4c1853b01cd847678e828555f83fe8
Elizabeth Thatcher-Adams <elizabeth.thatcher@live.com>
2026-09-22 09:57:43 -0500

    continuing DADA2 processing, updated script

--- scripts/04_dada2_pipeline.R
+++ scripts/04_dada2_pipeline.R
@@ -1,8 +1,11 @@
+###############################################
+# 1. Load Metadata & File Paths
+###############################################
+
 meta <- read.csv("data/raw/infant_fastq/infant_metadata/metadata.csv", stringsAsFactors = FALSE)
 
 fnFs <- setNames(meta$File1, meta$SampleID)
 fnRs <- setNames(meta$File2, meta$SampleID)
-
 
 meta$File1 <- file.path("data/raw/infant_fastq", meta$File1)
 meta$File2 <- file.path("data/raw/infant_fastq", meta$File2)
@@ -13,9 +16,10 @@ fnRs <- setNames(meta$File2, meta$SampleID)
 head(fnFs)
 head(fnRs)
 
-###############################
-# Set up DADA2 package
-###############################
+
+###############################################
+# 2. Load & Install DADA2
+###############################################
 
 # Install BiocManager if needed
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
@@ -27,15 +31,13 @@ if (!requireNamespace("dada2", quietly = TRUE)) {
   BiocManager::install("dada2")
 }
 
-# Load dada2
 library(dada2)
 
 
-###############################
-# Create filtered outputs
-###############################
+###############################################
+# 3. Create Filtered Output Directory
+###############################################
 
-# Create filtered directory if it doesn't exist
 if(!dir.exists("data/raw/infant_fastq/filtered")){
   dir.create("data/raw/infant_fastq/filtered")
 }
@@ -49,8 +51,17 @@ filtRs <- file.path("data/raw/infant_fastq/filtered",
 packageVersion("dada2")
 
 
+###############################################
+# 4. Quality Profiles
+###############################################
+
 plotQualityProfile(fnFs[1:2])
 plotQualityProfile(fnRs[1:2])
+
+
+###############################################
+# 5. Filtering & Trimming
+###############################################
 
 out <- filterAndTrim(fnFs, filtFs,
                      fnRs, filtRs,
@@ -64,9 +75,75 @@ out <- filterAndTrim(fnFs, filtFs,
 
 head(out)
 
+
+###############################################
+# 6. Dereplication
+###############################################
+
 derepFs <- derepFastq(filtFs)
 derepRs <- derepFastq(filtRs)
 
 names(derepFs) <- names(filtFs)
 names(derepRs) <- names(filtRs)
 
+
+###############################################
+# 7. Learn Error Rates
+###############################################
+
+errF <- learnErrors(filtFs, multithread=FALSE)
+errR <- learnErrors(filtRs, multithread=FALSE)
+
+plotErrors(errF, nominalQ=TRUE)
+plotErrors(errR, nominalQ=TRUE)
+
+
+###############################################
+# 8. Sample Inference (DADA)
+###############################################
+
+dadaFs <- dada(derepFs, err=errF, multithread=FALSE)
+dadaRs <- dada(derepRs, err=errR, multithread=FALSE)
+
+
+###############################################
+# 9. Merge Paired Reads
+###############################################
+
+mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs)
+
+dadaFs[[1]]  # Inspect first sample
+
+
+###############################################
+# 10. Construct Sequence Table
+###############################################
+
+seqtab <- makeSequenceTable(mergers)
+dim(seqtab)
+
+
+###############################################
+# 11. Remove Chimeras
+###############################################
+
+seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE)
+
+dim(seqtab.nochim)
+sum(seqtab.nochim) / sum(seqtab)
+
+
+###############################################
+# 12. Assign Taxonomy (SILVA v138)
+###############################################
+
+taxa <- assignTaxonomy(
+  seqtab.nochim,
+  "data/silva/silva_nr_v138_train_set.fa.gz",
+  multithread = FALSE
+)
+
+taxa <- addSpecies(
+  taxa,
+  "data/silva/silva_species_assignment_v138.fa.gz"
+)
